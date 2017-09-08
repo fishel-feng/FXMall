ade41a3a0cb3883021cb37153bd362ad5a9ae66d
fx109138 <1091381572@qq.com>
2017-09-08 15:02:35 +0800

    内容添加

---
+++ FXMall-content/FXMall-content-interface/src/main/java/com/fx/content/service/ContentService.java
@@ -0,0 +1,9 @@
+package com.fx.content.service;
+
+import com.fx.common.pojo.FXResult;
+import com.fx.pojo.TbContent;
+
+public interface ContentService {
+
+    FXResult addContent(TbContent content);
+}
