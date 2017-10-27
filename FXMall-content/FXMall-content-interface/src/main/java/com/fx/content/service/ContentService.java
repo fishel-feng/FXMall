7f170b674efff5621ffde57331a95855dbe5aa0b
fx109138 <1091381572@qq.com>
2017-10-27 13:05:54 +0800

    first commit

--- FXMall-content/FXMall-content-interface/src/main/java/com/fx/content/service/ContentService.java
+++
@@ -1,9 +0,0 @@
-package com.fx.content.service;
-
-import com.fx.common.pojo.FXResult;
-import com.fx.pojo.TbContent;
-
-public interface ContentService {
-
-    FXResult addContent(TbContent content);
-}
